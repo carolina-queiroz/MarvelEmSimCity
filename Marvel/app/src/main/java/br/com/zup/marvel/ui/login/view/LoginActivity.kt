99e4e04afffcfc5adf6d246abecfb38c39a582a9
joanascheer <scheer.joana@gmail.com>
2022-07-26 13:23:52 -0300

    Implementando Authentication

--- Marvel/app/src/main/java/br/com/zup/marvel/ui/login/view/LoginActivity.kt
+++ Marvel/app/src/main/java/br/com/zup/marvel/ui/login/view/LoginActivity.kt
@@ -1,12 +1,68 @@
 package br.com.zup.marvel.ui.login.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import br.com.zup.marvel.R
+import br.com.zup.marvel.USER_KEY
+import br.com.zup.marvel.databinding.ActivityLoginBinding
+import br.com.zup.marvel.domain.model.User
+import br.com.zup.marvel.ui.home.view.HomeActivity
+import br.com.zup.marvel.ui.login.viewmodel.LoginViewModel
+import br.com.zup.marvel.ui.register.view.RegisterActivity
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    private val viewModel: LoginViewModel by lazy {
+        ViewModelProvider(this)[LoginViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvCreateAccount.setOnClickListener {
+            goRegisterUser()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val user = getUserData()
+            viewModel.validateDataUser(user)
+        }
+
+        initObservers()
+
+    }
+
+    private fun goRegisterUser() {
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+
+    private fun getUserData(): User {
+        return User(
+            email = binding.etEmailLogin.text.toString(),
+            password = binding.etPasswordLogin.text.toString()
+        )
+    }
+
+    private fun initObservers() {
+        viewModel.loginState.observe(this) {
+            goHome(it)
+        }
+
+        viewModel.errorState.observe(this) {
+            Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun goHome(user: User) {
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra(USER_KEY, user)
+        }
+        startActivity(intent)
     }
 }
--- Marvel/app/src/main/java/br/com/zup/marvel/ui/register/view/RegisterActivity.kt
+++ Marvel/app/src/main/java/br/com/zup/marvel/ui/register/view/RegisterActivity.kt
@@ -1,12 +1,62 @@
 package br.com.zup.marvel.ui.register.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.zup.marvel.R
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
+import br.com.zup.marvel.*
+import br.com.zup.marvel.databinding.ActivityRegisterBinding
+import br.com.zup.marvel.domain.model.User
+import br.com.zup.marvel.domain.repository.AuthenticationRepository
+import br.com.zup.marvel.ui.home.view.HomeActivity
+import br.com.zup.marvel.ui.register.viewmodel.RegisterViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+    private val viewModel: RegisterViewModel by lazy {
+        ViewModelProvider(this)[RegisterViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegisterAccount.setOnClickListener {
+            val user = getUserData()
+            viewModel.validateDataUser(user)
+        }
+
+        initObservers()
     }
+
+    private fun initObservers() {
+        viewModel.registerState.observe(this) {
+            goHome(it)
+        }
+
+        viewModel.errorState.observe(this) {
+            Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getUserData(): User {
+        return User(
+            name = binding.etNameRegister.text.toString(),
+            email = binding.etEmailRegister.text.toString(),
+            password = binding.etPasswordRegister.text.toString()
+        )
+    }
+
+    private fun goHome(user: User) {
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra(USER_KEY, user)
+        }
+        startActivity(intent)
+    }
+
 }
